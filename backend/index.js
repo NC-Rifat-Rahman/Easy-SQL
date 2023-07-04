6f332ab97e5328ca2e233994b5c158d610cd3a47
NC-Rifat-Rahman <rahmanrifat61@gmail.com>
2023-07-04 12:22:44 +0600

    generate_query_complete

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const cors = require('cors');
+
+import generate from './generate.js';
+import cors from 'cors';
+import express from 'express';
 
 const app = express();
+
+app.use(express.json());
 
 app.use(cors());
 
@@ -10,6 +14,21 @@ const port = process.env.PORT || 3005;
 app.get("/",(req,res) =>
 {
     res.send("Hello world from our API");
+})
+
+// as generate function is async we also make it async
+app.post("/generate", async (req,res) =>
+{
+    const queryDescription = req.body.queryDescription;
+
+    try{
+        const sqlQuery = await generate(queryDescription);
+        res.json({ response: sqlQuery });
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).send("Internal Server error")
+    }
 })
 
 app.listen(port, () =>
